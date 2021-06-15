b21bd55be173cfdddac74ab1c345bd80764683b4
Akhmajon Makhsadov <akhmajon.makhsadov@dfki.de>
2021-06-16 01:15:43 +0200

    Cleaned up the folders, fixed the conflict related to ViveSR and Android build, improved networking scripts

---
+++ Assets/Framework/Scripts/Networking/SimpleNetworkManger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.Networking;
+
+public class SimpleNetworkManger : MonoBehaviour
+{
+    public string networkAddress = "192.168.0.211";
+    NetworkManager networkManager;
+
+    private void Awake()
+    {
+        networkManager = GetComponent<NetworkManager>();
+        networkManager.networkAddress = networkAddress;
+    }
+
+    public void StartServer()
+    {
+        networkManager.StartServer();
+    }
+
+    public void StartClient()
+    {
+        networkManager.StartClient();
+    }
+}
